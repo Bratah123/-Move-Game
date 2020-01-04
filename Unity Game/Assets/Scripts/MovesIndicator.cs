3d577f32c50828f10676e10c2a4018338beab50d
Bratah123 <58405975+Bratah123@users.noreply.github.com>
2020-01-03 18:54:57 -0800

    Added Moves UI, and Added Respawning After Max Use of Moves

---
+++ Unity Game/Assets/Scripts/MovesIndicator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovesIndicator : MonoBehaviour
+{
+    public GameObject MovesText;
+
+
+    void Update()
+    {
+        MovesText.GetComponent<Text>().text = "Moves: " + PlayerController.moves;
+    }
+}
--- Unity Game/Assets/Scripts/PlayerController.cs
+++ Unity Game/Assets/Scripts/PlayerController.cs
@@ -10,21 +10,27 @@ public class PlayerController : MonoBehaviour
 
     public float squareMovementHorizontal = 2f;
     public float squareMovementHorizontal2 = -2f;
+
     // How many squares they move up and down
+
     public float squareMovementVertical = 2f;
     public float squareMovementVertical2 = -2f;
 
     // maximum moves the player is allowed
 
-    public int moves;
+    public static int moves;
+    public int internalMoves = 5;
+    int originalMoves = 5;
     void Start()
     {
-       
+        moves = internalMoves;
     }
 
     // Update is called once per frame
     void Update()
     {
+        moves = internalMoves;
+
         Vector3 playerHorizontal = new Vector3(squareMovementHorizontal, 0f, 0f);
         Vector3 playerHorizontal2 = new Vector3(squareMovementHorizontal2, 0f, 0f);
         Vector3 playerVertical = new Vector3(0f, squareMovementVertical, 0f);
@@ -36,32 +42,77 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             player.Translate(playerHorizontal);
+            internalMoves--;
         }
         // left
         else if(Input.GetKeyDown(KeyCode.A))
         {
             player.Translate(playerHorizontal2);
+            internalMoves--;
         }
         // up
         else if (Input.GetKeyDown(KeyCode.W))
         {
             player.Translate(playerVertical);
+            internalMoves--;
         }
         // down
         else if (Input.GetKeyDown(KeyCode.S))
         {
             player.Translate(playerVertical2);
+            internalMoves--;
         }
 
         maxHeightWidth();
+        movesLogic();
     }
 
-    Vector3 maxPos = new Vector3(0f, 0f, 0f);
+    // The coordinates that respawns player back to beginning of level
+
+    Vector3 respawnPoint = new Vector3(0f, 0f, 0f);
     void maxHeightWidth()
     {
+
+        // if they past the boundary move them back to original spot.
         if(transform.position.x > 8)
         {
-            player.position = maxPos;
+            player.Translate(-2f, 0f, 0f);
+            internalMoves++;
         }
+        else if(transform.position.x < -8)
+        {
+            player.Translate(2f, 0f, 0f);
+            internalMoves++;
+        }
+        else if (transform.position.y > 2)
+        {
+            player.Translate(0f, -2f, 0f);
+            internalMoves++;
+        }
+        else if (transform.position.y < -4)
+        {
+            player.Translate(0f, 2f, 0f);
+            internalMoves++;
+        }
+    }
+
+    // if you run out of moves then respawn character
+    void movesLogic()
+    {
+        if(moves <= 0)
+        {
+            Respawn();
+        }
+    }
+
+    // resets the position of the player back to the middle and give them 2 more moves (Dying is rewarded)
+    void Respawn()
+    {
+        originalMoves += 2;
+        player.position = respawnPoint;
+
+
+        internalMoves = originalMoves;
+
     }
 }
