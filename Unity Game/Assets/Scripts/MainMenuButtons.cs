d1f75ca3e207c33c14a1b217e7e954c9e4d94e11
Bratah123 <58405975+Bratah123@users.noreply.github.com>
2020-01-06 20:59:45 -0800

    Added A Main Menu and Info Page

---
+++ Unity Game/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    // Loads the scene for all of the Levels
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Exits application of game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void InfoPage()
+    {
+        SceneManager.LoadScene("InfoPage");
+    }
+
+    // For the back button in InfoPage
+    // Probably not a smart idea to create another scene for the info page but whatever lol.
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
