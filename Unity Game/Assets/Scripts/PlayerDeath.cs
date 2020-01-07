6c05748e119ad7a8df6cdc69a12bb9e4ca2df8de
Bratah123 <58405975+Bratah123@users.noreply.github.com>
2020-01-07 15:49:48 -0800

    Added a Lava Floor and Death() function
    Death adds 2 moves and respawns player back to respawn point which is the bottom left corner.

--- Unity Game/Assets/Scripts/PlayerDeath.cs
+++ Unity Game/Assets/Scripts/PlayerDeath.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform player;
+    public Transform Lava;
+    int originalMoves = 1;
+ 
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.tag == "Lava")
+        {
+            Death();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Death()
     {
-        
+        Vector3 respawnCoord = new Vector3(-8f, -4f, 0f);
+        PlayerController.moves += 3;
+        player.transform.position = respawnCoord;
     }
 }
